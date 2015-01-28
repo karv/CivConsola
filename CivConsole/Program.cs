14575cae9412af664a1b0afd3a5657df552252bd
Edgar Carballo <karvayoEdgar@gmail.com>
2015-01-28 17:50:15 -0600

    Inicial
    Importar libciv

---
+++ CivConsole/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CivConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
