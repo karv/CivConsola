79f54ee60f04f4a0257947c2c789db703b2ec9e3
Edgar Carballo <karvayoEdgar@gmail.com>
2015-01-28 18:09:37 -0600

    Agregando el xml

--- CivConsole/Program.cs
+++ CivConsole/Program.cs
@@ -1,12 +1,39 @@
 using System;
+using Civ;
+using Global;
 
 namespace CivConsole
 {
     class MainClass
     {
+
+        static Civilizacion MyCiv;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            g_.CargaData();
+            g_.InicializarJuego();
+
+
+            MyCiv = g_.State.Civs[0];
+
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine().ToLower();
+                switch (input)
+                {
+                    case "ciencias":
+                        Console.WriteLine("Ciencias conocidas:");
+                        foreach (var x in MyCiv.Avances)
+                        {
+                            Console.WriteLine(x);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
