681ada4f7203345963a2d29f113d9d891a998b0c
Edgar Carballo <karvayoEdgar@gmail.com>
2015-03-18 16:25:02 -0600

    Pull civlib

--- CivConsole/Program.cs
+++ CivConsole/Program.cs
@@ -1,39 +1,64 @@
 using System;
 using Civ;
 using Global;
+using System.Threading;
 
 namespace CivConsole
 {
-    class MainClass
-    {
+	class MainClass
+	{
 
-        static Civilizacion MyCiv;
+		static Civilizacion MyCiv;
+		static DateTime LastUpdate;
 
-        public static void Main(string[] args)
-        {
-            g_.CargaData();
-            g_.InicializarJuego();
+		public static void Main(string[] args)
+		{
+			g_.CargaData();
+
+			g_.InicializarJuego();
 
 
-            MyCiv = g_.State.Civs[0];
+			MyCiv = g_.State.Civs[0];
+			/*
+			Armada a = new Armada();
+			a.AgregaUnidad(new Unidad(U));
+			MyCiv.Armadas.Add(a);
+			Armada b = new Armada();
+			b.AgregaUnidad(new Unidad(U));
+			g_.State.Civs[1].Armadas.Add(b);
 
-            string input;
-            while (true)
-            {
-                input = Console.ReadLine().ToLower();
-                switch (input)
-                {
-                    case "ciencias":
-                        Console.WriteLine("Ciencias conocidas:");
-                        foreach (var x in MyCiv.Avances)
-                        {
-                            Console.WriteLine(x);
-                        }
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    }
+			a.Pelea(b, 0.01f);
+            */
+
+			Thread Tk = new Thread(new ThreadStart(Ticker));
+			string input;
+			LastUpdate = DateTime.Now;
+			Tk.Start();
+			while (true)
+			{
+				input = Console.ReadLine().ToLower();
+				switch (input)
+				{
+					case "ciencias":
+						Console.WriteLine("Ciencias conocidas:");
+						foreach (var x in MyCiv.Avances)
+						{
+							Console.WriteLine(x);
+						}
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
+		static void Ticker()
+		{
+			DateTime N = DateTime.Now;
+			TimeSpan t = N - LastUpdate;
+			LastUpdate = N;
+
+			g_.Tick((float)t.TotalHours);
+		}
+	}
 }
